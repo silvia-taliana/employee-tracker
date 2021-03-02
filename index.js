d0b3b0cc306f2f062c5fabe0d54a0801aae2a2dc
Silvia Taliana <silviataliana2@gmail.com>
2021-03-02 21:46:26 +1030

    connecting mysql

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: 'root',
+    database: 'employees_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+});
+
+const start = () => {
+    console.log("successfully started my function")
+};
