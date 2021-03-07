07c28aa2005117986684b0f5b3824b9da53b0a13
Silvia Taliana <silviataliana2@gmail.com>
2021-03-07 16:42:13 +1030

    updating sql files to include foreign keys

--- mysql/employeetracker.sql
+++ mysql/employeetracker.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
@@ -15,23 +16,31 @@ salary DECIMAL NULL,
 department_id INT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (department_id) REFERENCES department(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
 id INT NOT NULL,
-first_name VARCHAR(30) NULL,
-last_name VARCHAR(30) NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 role_id INT NULL,
 manager_id INT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (role_id) REFERENCES role(id),
 FOREIGN KEY (manager_id) REFERENCES employee(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+FOREIGN KEY (role_id) REFERENCES role(id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
 );
 
-SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id
-FROM ((role
-INNER JOIN employee ON role.id=employee.role_id)
-INNER JOIN department ON role.department_id=department.id);
+-- view all employees
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, " ", manager.last_name) AS manager_name
+FROM employee
+LEFT JOIN role ON role.id=employee.role_id
+LEFT JOIN department ON role.department_id=department.id
+LEFT JOIN employee AS manager ON employee.manager_id=manager.id;
 
 SELECT * FROM department;
 SELECT * FROM role;
--- mysql/seed.sql
+++ mysql/seed.sql
@@ -1,35 +1,51 @@
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Stevie", "Nicks", 101, 2); 
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (5, "Freddy", "Mercury", 101, 2); 
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (101, "Software Engineer", 150000, 201);
-
 INSERT INTO department (id, name)
 VALUES (201, "Engineering");
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Elvis", "Presley", 102, 4); 
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (102, "Lead Engineer", 250000, 201);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (3, "Jackie", "Chan", 103, 4); 
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (103, "HR Manager", 200000, 202);
 
 INSERT INTO department (id, name)
 VALUES (202, "Human Resources");
 
-INSERT INTO employee (id, first_name, last_name, role_id)
-VALUES (4, "Keanu", "Reeves", 104); 
+INSERT INTO department (id, name)
+VALUES (203, "Management Team");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (101, "Software Engineer", 150000, 201);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (102, "Lead Engineer", 250000, 201);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (103, "HR Manager", 200000, 202);
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (104, "General Manager", 300000, 203);
 
-INSERT INTO department (id, name)
-VALUES (203, "Management Team");
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, "Stevie", "Nicks", 101); 
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (2, "Elvis", "Presley", 102); 
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (3, "Jackie", "Chan", 103); 
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (4, "Keanu", "Reeves", 104); 
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (5, "Freddy", "Mercury", 101); 
+
+UPDATE employee
+SET employee.manager_id = 2
+WHERE employee.id = 1; 
+
+UPDATE employee
+SET employee.manager_id = 4
+WHERE employee.id = 2; 
+
+UPDATE employee
+SET employee.manager_id = 4
+WHERE employee.id = 3; 
+
+UPDATE employee
+SET employee.manager_id = 2
+WHERE employee.id = 5; 
